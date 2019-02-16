d2a64efceb61203e930522fae80a82b491450e34
huangzhen <danceiny@gmail.com>
2019-02-16 21:57:22 +0800

    nil interface check

--- nil.go
+++ nil.go
@@ -5,11 +5,11 @@ import "reflect"
 func InterfaceHasNilValue(actual interface{}) bool {
     value := reflect.ValueOf(actual)
     kind := value.Kind()
-    nilable := kind == reflect.Slice ||
+    nilable := kind == reflect.Ptr ||
+        kind == reflect.Slice ||
         kind == reflect.Chan ||
-        kind == reflect.Func ||
-        kind == reflect.Ptr ||
-        kind == reflect.Map
+        kind == reflect.Map ||
+        kind == reflect.Func
 
     // Careful: reflect.Value.IsNil() will panic unless it's an interface, chan, map, func, slice, or ptr
     // Reference: http://golang.org/pkg/reflect/#Value.IsNil
